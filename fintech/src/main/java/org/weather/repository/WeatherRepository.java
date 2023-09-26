a0168204df92cbf5718408a2e47931b83335e5ab
MadPashkaZhuk <paskam1note@gmail.com>
2023-09-26 10:21:11 +0300

    added spring dependencies and controller, repository packages

---
+++ fintech/src/main/java/org/weather/repository/WeatherRepository.java
@@ -0,0 +1,8 @@
+package org.weather.repository;
+
+import org.weather.entity.Weather;
+
+import java.util.List;
+
+public interface WeatherRepository {
+}
---
+++ fintech/src/main/java/org/weather/repository/impl/InMemoryWeatherRepository.java
@@ -0,0 +1,6 @@
+package org.weather.repository.impl;
+
+import org.weather.repository.WeatherRepository;
+
+public class InMemoryWeatherRepository implements WeatherRepository {
+}
