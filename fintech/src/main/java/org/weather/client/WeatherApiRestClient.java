42885b9cb8cccf8027ba845d4bdebf890d062b13
MadPashkaZhuk <paskam1note@gmail.com>
2023-10-09 21:27:31 +0300

    fixed issues from PR#4

--- fintech/src/main/java/org/weather/client/WeatherApiRestClient.java
+++ fintech/src/main/java/org/weather/client/WeatherApiRestClient.java
@@ -1,8 +1,9 @@
 package org.weather.client;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,45 +13,39 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.weather.dto.WeatherApiDTO;
 import org.weather.dto.WeatherApiErrorDTO;
-import org.weather.exception.weatherapi.WeatherApiDisabledKeyException;
-import org.weather.exception.weatherapi.WeatherApiIncorrectQueryException;
-import org.weather.exception.weatherapi.WeatherApiUnknownException;
-import org.weather.exception.weatherapi.WeatherApiWrongKeyException;
+import org.weather.exception.weatherapi.*;
 import org.weather.service.impl.DefaultWeatherControllerService;
-
-import java.util.Locale;
+import org.weather.utils.ErrorCodeHelper;
+import org.weather.utils.MessageSourceWrapper;
+import org.weather.utils.enums.ErrorCodeEnum;
+import org.weather.utils.enums.WeatherMessageEnum;
 
 @Component
+@Setter
+@ConfigurationProperties(prefix = "weather.api")
 public class WeatherApiRestClient {
     private final DefaultWeatherControllerService weatherControllerService;
-    private final String weatherApiInvalidUrlMessage = "weatherapi.invalid.url.message";
-    private final String weatherApiWrongKeyMessage = "weatherapi.wrong.key.message";
-    private final String weatherApiDisabledKeyMessage = "weatherapi.disabled.key.message";
-    private final String weatherApiUnknownExceptionMessage = "weatherapi.unknown.exception.message";
-    private final int unknownErrorCode;
     private final RestTemplate restTemplate;
-    private final MessageSource messageSource;
-    private final String weatherApiKey;
-    private final String weatherApiUrlPath;
+    private final MessageSourceWrapper messageSource;
+    private final ErrorCodeHelper errorCodeHelper;
+    private String key;
+    private String url;
 
     public WeatherApiRestClient(DefaultWeatherControllerService weatherControllerService,
                                 @Qualifier("WeatherApiRestTemplate") RestTemplate restTemplate,
-                                MessageSource messageSource,
-                                @Value("${weather.api.key}") String weatherApiKey,
-                                @Value("${weather.api.url}") String weatherApiUrlPath,
-                                @Value("${weather.api.unknown.error.code}") int unknownErrorCode) {
+                                MessageSourceWrapper messageSource,
+                                ErrorCodeHelper errorCodeHelper) {
         this.weatherControllerService = weatherControllerService;
         this.restTemplate = restTemplate;
         this.messageSource = messageSource;
-        this.weatherApiKey = weatherApiKey;
-        this.weatherApiUrlPath = weatherApiUrlPath;
-        this.unknownErrorCode = unknownErrorCode;
+        this.errorCodeHelper = errorCodeHelper;
     }
 
+    @RateLimiter(name = "weatherApiCurrent")
     public double getTemperatureFromWeatherApi(String regionName) {
-        ResponseEntity<WeatherApiDTO> responseEntity = null;
-        String finalPath = UriComponentsBuilder.fromUriString(weatherApiUrlPath)
-                .queryParam("key", weatherApiKey)
+        ResponseEntity<WeatherApiDTO> responseEntity;
+        String finalPath = UriComponentsBuilder.fromUriString(url)
+                .queryParam("key", key)
                 .queryParam("q", regionName)
                 .queryParam("aqi", "no")
                 .toUriString();
@@ -64,32 +59,32 @@ public class WeatherApiRestClient {
             WeatherApiDTO weatherApiDTO = responseEntity.getBody();
             return this.weatherControllerService.getTemperatureFromWeatherApi(weatherApiDTO);
         } catch (HttpStatusCodeException ex) {
-            WeatherApiErrorDTO weatherApiErrorDTO = ex.getResponseBodyAs(WeatherApiErrorDTO.class);
-            int errorCode = weatherApiErrorDTO.getError().getCode();
-            if(ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                throw new WeatherApiIncorrectQueryException(HttpStatus.BAD_REQUEST,
-                        getMessageFromMessageSource(weatherApiInvalidUrlMessage), errorCode);
-            }
-            if(ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                throw new WeatherApiWrongKeyException(HttpStatus.UNAUTHORIZED,
-                        getMessageFromMessageSource(weatherApiWrongKeyMessage), errorCode);
-            }
-            if(ex.getStatusCode() == HttpStatus.FORBIDDEN) {
-                throw new WeatherApiDisabledKeyException(HttpStatus.FORBIDDEN,
-                        getMessageFromMessageSource(weatherApiDisabledKeyMessage), errorCode);
-            }
-            else {
-                throw new WeatherApiUnknownException(HttpStatus.REQUEST_TIMEOUT,
-                        getMessageFromMessageSource(weatherApiUnknownExceptionMessage), unknownErrorCode);
-            }
+            throw getWeatherApiExceptionFromHttpException(ex);
         } catch (Throwable ex) {
             throw new WeatherApiUnknownException(HttpStatus.REQUEST_TIMEOUT,
-                    getMessageFromMessageSource(weatherApiUnknownExceptionMessage), unknownErrorCode);
+                    messageSource.getMessageCode(WeatherMessageEnum.UNKNOWN_EXCEPTION),
+                    errorCodeHelper.getCode(ErrorCodeEnum.UNKNOWN_ERROR_CODE));
         }
     }
 
-    private String getMessageFromMessageSource(String messageCode) {
-        return messageSource.getMessage(messageCode, null, Locale.getDefault());
+    private BaseWeatherApiException getWeatherApiExceptionFromHttpException(HttpStatusCodeException ex) {
+        WeatherApiErrorDTO weatherApiErrorDTO = ex.getResponseBodyAs(WeatherApiErrorDTO.class);
+        int errorCode = weatherApiErrorDTO.getError().getCode();
+        BaseWeatherApiException exception;
+        if(ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
+            exception = new WeatherApiIncorrectQueryException(HttpStatus.BAD_REQUEST,
+                    messageSource.getMessageCode(WeatherMessageEnum.INVALID_URL), errorCode);
+        } else if(ex.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+            exception = new WeatherApiWrongKeyException(HttpStatus.UNAUTHORIZED,
+                    messageSource.getMessageCode(WeatherMessageEnum.API_KEY_INVALID), errorCode);
+        } else if(ex.getStatusCode() == HttpStatus.FORBIDDEN) {
+            exception = new WeatherApiDisabledKeyException(HttpStatus.FORBIDDEN,
+                    messageSource.getMessageCode(WeatherMessageEnum.API_KEY_DISABLED), errorCode);
+        } else {
+            exception = new WeatherApiUnknownException(HttpStatus.REQUEST_TIMEOUT,
+                    messageSource.getMessageCode(WeatherMessageEnum.UNKNOWN_EXCEPTION), errorCode);
+        }
+        return exception;
     }
 }
 
--- fintech/src/main/java/org/weather/controller/WeatherApiController.java
+++ fintech/src/main/java/org/weather/controller/WeatherApiController.java
@@ -1,37 +1,21 @@
 package org.weather.controller;
 
-import io.github.resilience4j.ratelimiter.RequestNotPermitted;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.weather.client.WeatherApiRestClient;
-import org.weather.exception.weatherapi.WeatherApiTooManyRequestsException;
-
-import java.util.Locale;
 
 @Controller
 @RequestMapping("/external/weatherapi")
 public class WeatherApiController {
     private final WeatherApiRestClient weatherApiRestClient;
-    private final MessageSource messageSource;
-    private final int tooManyRequestsErrorCode;
-    private final String tooManyRequestsMessage = "weatherapi.too.many.requests.message";
-
-    public WeatherApiController(WeatherApiRestClient weatherApiRestClient,
-                                MessageSource messageSource,
-                                @Value("${weather.api.too.many.requests.code}") int tooManyRequestsErrorCode) {
+    public WeatherApiController(WeatherApiRestClient weatherApiRestClient) {
         this.weatherApiRestClient = weatherApiRestClient;
-        this.messageSource = messageSource;
-        this.tooManyRequestsErrorCode = tooManyRequestsErrorCode;
     }
 
     @Operation(summary = "Get temperature from weatherapi.com",
@@ -45,15 +29,8 @@ public class WeatherApiController {
             @ApiResponse(responseCode = "429", description = "Too many requests: Rate Limit is exceeded")
     })
     @GetMapping("/{regionName}")
-    @RateLimiter(name = "weatherApiCurrent", fallbackMethod = "fallbackMethod")
     public ResponseEntity<?> handleGetTemperature(@PathVariable("regionName") String regionName) {
         return ResponseEntity.ok()
                 .body(this.weatherApiRestClient.getTemperatureFromWeatherApi(regionName));
-    }
-
-    public ResponseEntity<?> fallbackMethod(RequestNotPermitted requestNotPermitted) {
-        throw new WeatherApiTooManyRequestsException(HttpStatus.TOO_MANY_REQUESTS, messageSource
-                .getMessage(tooManyRequestsMessage, null, Locale.getDefault()),
-                tooManyRequestsErrorCode);
     }
 }
