1979a9454ce3cc30faf52dd1833b34ab0b9e0f96
MadPashkaZhuk <paskam1note@gmail.com>
2023-10-14 23:30:35 +0300

    updated weatherapi contoller and readme

---
+++ fintech/src/main/java/org/weather/service/WeatherApiService.java
@@ -0,0 +1,12 @@
+package org.weather.service;
+
+import org.springframework.stereotype.Service;
+import org.weather.dto.WeatherDTO;
+import org.weather.dto.weatherapi.WeatherApiDTO;
+
+@Service
+public class WeatherApiService {
+    public double getTemperatureFromWeatherApi(WeatherApiDTO weatherApiDTO) {
+        return weatherApiDTO.getCurrent().getTemperatureInCelsius();
+    }
+}
